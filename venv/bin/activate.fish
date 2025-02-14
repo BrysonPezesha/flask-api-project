84edff5212c427bbab17bd29753336780bc8f862
BrysonPezesha <bryson@pezesha.com>
2025-02-14 20:52:26 +0300

    Added new files

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/bryson/projects/flask-llm-bank-extraction/venv
+set -gx VIRTUAL_ENV /home/bryson/bryson/flask-llm-bank-extraction/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
